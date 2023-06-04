7ef3180bcf3b97d18e6fd53fbccb961316202784
Kilian Cannet <kkiillcc@gmail.com>
2023-06-04 20:17:12 +0200

    Remove undeleted file by git

--- NextJS/src/app/project/[id]/loading.tsx
+++
@@ -1,6 +0,0 @@
-//will show when loading the project page
-export default function Loading(){
-    return <div className="align-center font-mono flex min-h-screen  items-center justify-center p-24">
-        <p>Loading ...</p>
-    </div>
-}
